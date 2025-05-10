399eea69d9c0db94447141e936ebff34c9c31740
famibee <famibee@gmail.com>
2025-05-10 22:33:18 +0900

    v2.13.14: ライブラリ更新

--- core/web4webpack.js
+++ core/web4webpack.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // 変更後は「npm run webpack:dev」
 
 const hPlg = {};
-import h from './plugin.js';
+import h from './plugin.json';
 for (const nm in h) hPlg[nm] = require(`./plugin/${nm}`);
 
 import {SysWeb} from '@famibee/skynovel/web';
